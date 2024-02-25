d327b1b9172af3e3d829d459fe7d296fa7158009
Samuel Souza <smksouza.dev@gmail.com>
2024-02-24 21:33:32 -0300

    Getters e Setters das entidades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -7,12 +7,37 @@ interface AnswerProps {
     questionId: UniqueEntityID 
     content: string
     createdAt: Date
-    updateAt?: Date
+    updatedAt?: Date
 }
 
 export class Answer extends Entity<AnswerProps> {
+    get authorId() {
+        return this.props.authorId
+    }
+    get questionId() {
+        return this.props.questionId
+    }
     get content() {
         return this.props.content
+    }
+    get createdAt() {
+        return this.props.createdAt
+    }
+    get updatedAt() {
+        return this.props.updatedAt
+    }
+
+    get excerpt() {
+        return this.content.substring(0, 120).trimEnd().concat('...')
+    }
+
+    private touch() {
+        this.props.updatedAt = new Date()
+    }
+
+    set content(content: string) {
+        this.props.content = content
+        this.touch()
     }
 
     static create(
