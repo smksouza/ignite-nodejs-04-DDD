82576d88c79318ce1f83931a074618491370a75f
Samuel Souza <smksouza.dev@gmail.com>
2024-03-02 19:21:07 -0300

    Caso de uso:Listar respostas

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
@@ -10,6 +11,14 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     if (!answer) {
       return null
     }
+
+    return answer
+  }
+
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const answer = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
 
     return answer
   }
